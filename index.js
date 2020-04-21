d50785573e968bbad0d62b101aadeac8c14739d4
GTSorho <you@example.com>
2020-04-21 09:29:52 +0000

    21/04/2020 update

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ var path = require('path');
 var cors = require('cors');
 app.use(cors());
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+
 app.use(express.static(path.join(__dirname)));
 app.use("/css", express.static(__dirname));
 app.use("/media", express.static(__dirname + '/media'));
