9554b470c3b2deed2cc2998b31987c4c99cc8546
GTSorho <you@example.com>
2020-05-01 02:04:43 +0000

    repaired 30/4/2020

--- js/socket.js
+++ js/socket.js
@@ -1,61 +1,105 @@
 
+$(document).ready(function(){
+
   
-    // Enable pusher logging - don't include this in production
-    // Pusher.logToConsole = true;
 
-    var pusher = new Pusher('8757fa8b9d80a2c77f4b');
-    var tell;
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data) {
-      // console.log(data)
-      if(data.message.status == 'plural'){
-      $.each(data.message.message, function (i) {
-        myChart.data.labels.push(data.message.message[i].created_at);
-        myChart.data.datasets[0].data.push(data.message.message[i].temperature);
-        myChart.data.datasets[1].data.push(data.message.message[i].humidity);
-        myChart.data.datasets[2].data.push(data.message.message[i].rain);
-        myChart.data.datasets[3].data.push(data.message.message[i].moisture);
-        myChart.data.datasets[4].data.push(data.message.message[i].light);
-        myChart.update();
-      })
-      $('#temp_val').html(data.message.singleData.temperature)
-      $('#humidity_val').html(data.message.singleData.humidity)
-      $('#light_val').html(data.message.singleData.light)
-      $('#rain_val').html(data.message.singleData.rain)
-      $('#moisture_val').html(data.message.singleData.moisture)
-
-    }else if(data.message.status == 'single'){
-      myChart.data.labels.push(data.message.message.created_at);
-      myChart.data.datasets[0].data.push(data.message.message.temperature);
-      myChart.data.datasets[1].data.push(data.message.message.humidity);
-      myChart.data.datasets[2].data.push(data.message.message.rain);
-      myChart.data.datasets[3].data.push(data.message.message.moisture);
-      myChart.data.datasets[4].data.push(data.message.message.light);
-      myChart.update();
-
-      $('#temp_val').html(data.message.message.temperature)
-      $('#humidity_val').html(data.message.message.humidity)
-      $('#light_val').html(data.message.message.light)
-      $('#rain_val').html(data.message.message.rain)
-      $('#moisture_val').html(data.message.message.moisture)
-
-    }    
-      
-    });
+  var token = localStorage.getItem('access_token')
+  var user_id = localStorage.getItem('user_id')
+console.log(user_id)
+console.log(token)
 
 
 
+  $.ajax({
+  url: "https://prefarmapi.herokuapp.com/api/espdata",
+  method: "get",
+  headers: {
+    "Authorization": `Bearer ${token}`,
+  },
+  success: function (response) {
+ console.log(response)
+  }
+})
+
+// $.ajax({
+//   url: "https://prefarmapi.herokuapp.com/api/espdata/"+user_id,
+//   method: "delete",
+//   headers: {
+//     "Authorization": `Bearer ${token}`,
+//   },
+//   success: function (response) {
+//  console.log(response)
+//   }
+// })
+
+var x
+var pusher = new Pusher('6fbd3a4d78bbe2c53fbd');
+var channel = pusher.subscribe('my-channel'+ user_id);
+channel.bind('my-event', function(data) {
+  console.log(data)
+  
+  if(data.message.status == 'plural'){
+  $.each(data.message.message, function (i) {
+    myChart.data.labels.push(data.message.message[i].created_at);
+    myChart.data.datasets[0].data.push(data.message.message[i].temperature);
+    myChart.data.datasets[1].data.push(data.message.message[i].humidity);
+    myChart.data.datasets[2].data.push(data.message.message[i].rain);
+    myChart.data.datasets[3].data.push(data.message.message[i].moisture);
+    myChart.data.datasets[4].data.push(data.message.message[i].light);
+    myChart.update();
+  })
+
+  x = data.message.message.length
+ for(i=20; i < x; i++){
+  myChart.data.labels.shift();
+  myChart.data.datasets[0].data.shift();
+  myChart.data.datasets[1].data.shift();
+  myChart.data.datasets[2].data.shift();
+  myChart.data.datasets[3].data.shift();
+  myChart.data.datasets[4].data.shift();
+  myChart.update();
+ }
+
+  $('#temp_val').html(data.message.singleData.temperature)
+  $('#humidity_val').html(data.message.singleData.humidity)
+  $('#light_val').html(data.message.singleData.light)
+  $('#rain_val').html(data.message.singleData.rain)
+  $('#moisture_val').html(data.message.singleData.moisture)
+
+}else if(data.message.status == 'single'){
+
+  x=x+1
+  console.log(x)
+
+  myChart.data.labels.push(data.message.message.created_at);
+  myChart.data.datasets[0].data.push(data.message.message.temperature);
+  myChart.data.datasets[1].data.push(data.message.message.humidity);
+  myChart.data.datasets[2].data.push(data.message.message.rain);
+  myChart.data.datasets[3].data.push(data.message.message.moisture);
+  myChart.data.datasets[4].data.push(data.message.message.light);
+  myChart.update();
+
+  if(x>20){
+    myChart.data.labels.shift();
+    myChart.data.datasets[0].data.shift();
+    myChart.data.datasets[1].data.shift();
+    myChart.data.datasets[2].data.shift();
+    myChart.data.datasets[3].data.shift();
+    myChart.data.datasets[4].data.shift();
+    myChart.update();
+   }
+   
 
 
+  $('#temp_val').html(data.message.message.temperature)
+  $('#humidity_val').html(data.message.message.humidity)
+  $('#light_val').html(data.message.message.light)
+  $('#rain_val').html(data.message.message.rain)
+  $('#moisture_val').html(data.message.message.moisture)
+
+}    
+  
+});
 
 
-
-
-
-
-
-        // console.log(data.message[i].temperature)
-        // console.log(data.message[i].humidity)
-        // console.log(data.message[i].rain);
-        // console.log(data.message[i].light);
-        // console.log(data.message[i].moisture);
+})
